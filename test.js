4a94831b137477458aecfdbc7a7bd9ce79154128
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 21:19:00 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,23 +1,18 @@
-'use strict';
-var test = require('ava');
-var stableFn = require('stable-fn');
-var fn = require('./');
+import test from 'ava';
+import stableFn from 'stable-fn';
+import fn from './';
 
-var fixture = {
+const fixture = {
 	foo: 'a',
 	bar: 'b',
 	baz: 'c',
 	faz: 'd'
 };
 
-test(function (t) {
-	t.assert(!stableFn(function () {
-		return fn(fixture);
-	}));
+test(t => {
+	t.false(stableFn(() => fn(fixture)));
 
-	for (var i = 0; i < 1000; i++) {
-		t.assert(typeof fn(fixture) === 'string');
+	for (let i = 0; i < 1000; i++) {
+		t.is(typeof fn(fixture), 'string');
 	}
-
-	t.end();
 });
